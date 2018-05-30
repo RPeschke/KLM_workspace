75d4ea7eea108d18f26408a8fa4a9c463b3e6a74
rpeschke <richard.peschke@desy.de>
2018-05-29 21:50:13 -1000

    extracted some more functions

--- src/tx_ethparse1_ck.cpp
+++ src/tx_ethparse1_ck.cpp
@@ -7,6 +7,8 @@
 #include <vector>
 
 
+typedef std::pair<int,int> interval_t;
+
 const int NSAMP         = 32*4;
 const int MEMORY_DEPTH  = 512;
 const int NASICS        = 10;
@@ -63,39 +65,55 @@ friend std::ostream& operator<<(std::ostream& out, const MBevent& evt){
 
  //----FIND BUFFER POSITION FOR START OF PACKAGE----//
 
-  int  FIND_BUFFER_POSITION_FOR_START_OF_PACKAGE(const  unsigned char* buff,  int pos1,long size){
-        while (
-            (pos1 < (size-16))
-            && (!((buff[pos1]=='w' && buff[pos1+1]=='a' && buff[pos1+2]=='v' && buff[pos1+3]=='e')) // for OutMode==0 //ex. wave-->-------\    |pos1    --------         pos2\       (all utf 8-bit)
-            &&  !((buff[pos1]=='c' && buff[pos1+1]=='h' && buff[pos1+2]=='r' && buff[pos1+3]=='g'))) // for OutMode==1//sdf0k\Uffffffffdf\Uffffffffet\Uffffffffk\Uffffffffwq\Uffffffff\Uffffffffjwave\Uffffffff\Uffffffff\Uffffffff\Uffffffffu\Uffffffff57\Uffffffff\Ufffffffffm\Uffffffff/\Uffffffff08\Uffffffff\Uffffffff\Uffffffff\Uffffffffhrg=\Uffffffff3\Uffffffff\Uffffffff\Uffffffff1\Uffffffff\Uffffffffhn\Uffffffffhk\Uffffffff
-              ) {
+int  FIND_BUFFER_POSITION_FOR_START_OF_PACKAGE(const  unsigned char* buff,  int pos1,long size){
+    while (
+        (pos1 < (size-16))
+          && (!((buff[pos1]=='w' && buff[pos1+1]=='a' && buff[pos1+2]=='v' && buff[pos1+3]=='e')) // for OutMode==0 //ex. wave-->-------\    |pos1    --------         pos2\       (all utf 8-bit)
+          &&  !((buff[pos1]=='c' && buff[pos1+1]=='h' && buff[pos1+2]=='r' && buff[pos1+3]=='g'))) // for OutMode==1//sdf0k\Uffffffffdf\Uffffffffet\Uffffffffk\Uffffffffwq\Uffffffff\Uffffffffjwave\Uffffffff\Uffffffff\Uffffffff\Uffffffffu\Uffffffff57\Uffffffff\Ufffffffffm\Uffffffff/\Uffffffff08\Uffffffff\Uffffffff\Uffffffff\Uffffffffhrg=\Uffffffff3\Uffffffff\Uffffffff\Uffffffff1\Uffffffff\Uffffffffhn\Uffffffffhk\Uffffffff
+            ) {
             pos1++;
-        }
+    }
        
     return pos1;
-  }
+}
 
+interval_t Find_package_in_buffer(const  unsigned char* buff, const interval_t interval_v, long size){
+    int pos1 = interval_v.first;
+    pos1 = FIND_BUFFER_POSITION_FOR_START_OF_PACKAGE(buff,pos1,size);
+    pos1 += 4; // Increment both place holders past package markers identifed above.
+
+    int pos2 = FIND_BUFFER_POSITION_FOR_START_OF_PACKAGE(buff,pos1,size);
+
+    return interval_t(pos1,pos2);
+}
+bool is_valid_package(const  unsigned char* buff, const interval_t interval_v){
+    int pos1 = interval_v.first;
+    return (buff[pos1]==0xFE && buff[pos1+4]==0xCF && (buff[pos1+8]&0xF0)==0xD0);
+}
 bool end_of_package_marker(const unsigned char* buff,int pos1){
      // End of package marked by hex words "FACEFACE = \Uffffffff?
     return ((buff[pos1] == 0xFA) && (buff[pos1+1] == 0xCE) && (buff[pos1+2] == 0xFA) && (buff[pos1+3] == 0xCE)); 
 }
 
 //----CONVERT DATA FROM 8-BIT BUFFER TO 32-BIT BUFFER----//
-int CONVERT_DATA_FROM_8_BIT_BUFFER_TO_32_BIT_BUFFER(const unsigned char* buff, int& pos1, const int pos2,  unsigned int*  buffer_uint){
-    int wordNum=0;
+interval_t CONVERT_DATA_FROM_8_BIT_BUFFER_TO_32_BIT_BUFFER(const unsigned char* buff,const interval_t interval_v,  std::vector<unsigned int>&  buffer_uint){
+    buffer_uint.clear();
     int posRead=0;
+    int pos1 = interval_v.first;
     while (
-    (pos1<pos2)  && !end_of_package_marker(buff,pos1)) {
+    (pos1< interval_v.second)  && !end_of_package_marker(buff,pos1)) {
         if (posRead%4==0) { // Fill waveform buffer.
-            buffer_uint[wordNum]=((unsigned int)buff[pos1+0])<<24 | ((unsigned int)buff[pos1+1])<<16 | ((unsigned int)buff[pos1+2])<<8 | ((unsigned int)buff[pos1+3]);
-            wordNum++;
+            buffer_uint.push_back( 
+                ((unsigned int)buff[pos1+0])<<24 | ((unsigned int)buff[pos1+1])<<16 | ((unsigned int)buff[pos1+2])<<8 | ((unsigned int)buff[pos1+3]) 
+            );
+            
         }
         posRead++;
         pos1++; // When at end of while-loop, pos1 should be either at the word FACEFACE or at start of next package?
     }
             
     
-    return wordNum;
+    return interval_t(pos1,interval_v.second);
 }
 
 void PRINT_PSEUDO_STATUS_BAR_TO_TERMINAL(int eventCounter){
@@ -108,7 +126,7 @@ void PRINT_PSEUDO_STATUS_BAR_TO_TERMINAL(int eventCounter){
 
 
 //----CHECK FOR FEATURE EXTRACTION --  INDICATED BY "CB" VALUE----//
-void CHECK_FOR_FEATURE_EXTRACTION(unsigned int* buffer_uint, const int offset,const int count, MBevent& evt){
+void CHECK_FOR_FEATURE_EXTRACTION(const std::vector<unsigned int>& buffer_uint, const int offset,const int count, MBevent& evt){
     if ((buffer_uint[offset+count]& 0xFF000000)==0xCB000000) { // note: current usage is 31 bits
         int chanNum                 = (0x00F00000 & buffer_uint[offset+count])>>20;
         evt.PeakTime[chanNum]  = buffer_uint[offset+count]>>12 & 0x7F; // 7 bits for sample number
@@ -116,7 +134,91 @@ void CHECK_FOR_FEATURE_EXTRACTION(unsigned int* buffer_uint, const int offset,co
     }
 }
 
+//----CHECK FOR SAMPLE -- INDICATED BY "BD" VALUE----//
+bool CHECK_FOR_SAMPLE(const std::vector<unsigned int>& buffer_uint, const int offset,const int count,const int chanNum, const int window,  MBevent& evt){
+    // current usage is 31 bits
+    if ((buffer_uint[offset+count] & 0xFF000000) == 0xBD000000) {
+        const int sampNum     = ((buffer_uint[offset+count]>>12) & 0x0000001F)+window*32; // word #3 & up: read 5 bits
+        int BDval   =  (buffer_uint[offset+count]>>24) & 0x000000FF; // word #3 & up: read 8 bits (top 8 bits)
+        // make sure info is still valid before extracting samples---must pass these four conditions
+        if (evt.AddNum >= MEMORY_DEPTH  || evt.ASIC > NASICS || sampNum >= NSAMP || BDval != 0xBD) {
+            cout << "INVALID SAMPLE INFO, SKIPPING "    << "\n"
+                 << "Loop counter: "    << count        << "\n"
+                 << "Address number: "  << evt.AddNum  << "\n"
+                 << "ASIC number: "     << evt.ASIC    << "\n"
+                 << "Sample number: "   << sampNum      << "\n"
+                 << "BD value: "        << BDval        << "\n";
+            
+            return true;
+        }
+        else {
+            evt.Sample[chanNum][sampNum] = ((buffer_uint[offset+count]) & 0x00000FFF); // word #3 & up: read 12 bits (bottom 12 bits)
+        }
+    }
+    return false;
+}
 
+ //----CHECK FOR TRIGGER BITS -- INDICATED BY "CA" WORD FOLLOWED BY "C7" WORD----//
+void CHECK_FOR_TRIGGER_BITS(const std::vector<unsigned int>& buffer_uint,const int offset,const int count, std::ostream& out){
+    const int n_ca_max=32; // total of 64 hex words, 32 each of "CAXXXXXX" and "C7XXXXXX"
+    int n_ca                         = 0;
+    unsigned int CA_val[n_ca_max]    = {0};
+    unsigned int CA_raw1[n_ca_max]   = {0};
+    unsigned int CA_raw2[n_ca_max]   = {0};
+    unsigned int CA_win[n_ca_max]    = {0};
+    unsigned int CA_tbs[n_ca_max][4] = {{0}};
+    // note: (CA)(23 downto 17)(7 downto 0) and (C7)(23 downto 20) are only stored in raw's
+    if ((buffer_uint[offset+count] & 0xFF000000) == 0xCA000000) {
+        if ((buffer_uint[offset+count+1] & 0xFF000000) == 0xC7000000) {
+            if (n_ca < n_ca_max) {
+                CA_raw1[n_ca]   =  buffer_uint[offset+count  ]; // store "CA" word just in case
+                CA_raw2[n_ca]   =  buffer_uint[offset+count+1]; // store "C7" word just in case
+                CA_val[n_ca]    =  buffer_uint[offset+count+1] & 0x000FFFFF; // (C7) 19 downto 0 // redundant
+                CA_tbs[n_ca][0] = (buffer_uint[offset+count+1]>>0 ) & 0x0000001F; // (C7) 4 downto 0
+                CA_tbs[n_ca][1] = (buffer_uint[offset+count+1]>>5 ) & 0x0000001F; // (C7) 9 downto 5
+                CA_tbs[n_ca][2] = (buffer_uint[offset+count+1]>>10) & 0x0000001F; // (C7) 14 downto 10
+                CA_tbs[n_ca][3] = (buffer_uint[offset+count+1]>>15) & 0x0000001F; // (C7) 19 downto 15
+                CA_win[n_ca]    = (buffer_uint[offset+count  ] & 0x0001FF00) >> 8; // (CA) 16 downto 8
+                out << CA_raw1[n_ca]   << "\t";
+                out << CA_raw2[n_ca]   << "\t";
+                out << CA_val[n_ca]    << "\t";
+                out << CA_win[n_ca]    << "\t";
+                out << CA_tbs[n_ca][0] << "\t";
+                out << CA_tbs[n_ca][1] << "\t";
+                out << CA_tbs[n_ca][2] << "\t";
+                out << CA_tbs[n_ca][3] << "\n";
+            }
+            n_ca++; //useless??
+        }
+    }
+ }
+void extract_package_header(const std::vector<unsigned int>& buffer_uint, MBevent& evt){
+        //----FOR EACH 32-bit WORD, EXTRACT DATA USING BIT-WISE & OPERATORS----//
+    evt.AddNum     =   (buffer_uint[0])      & 0x000001FF ;   // word #0: read 5 bits
+    evt.WrAddNum   =   (buffer_uint[0]>>9)   & 0x000001FF ;   // word #0: read 5 bits
+    evt.ASIC       =  ((buffer_uint[0]>>20)  & 0x0000000F)-1; // word #0: read 4 bits
+    evt.EvtNum     =   (buffer_uint[1])      & 0x00FFFFFF ;   // word #1: read 24 bits
+    evt.Wctime     =   (buffer_uint[2])      & 0x0FFFFFFF ;   // word #2: read 28 bits
+}
+
+
+//----LOOP OVER CURRENT PACKAGE AND EXTRACT DATA----//
+void extract_package_body(const std::vector<unsigned int>&  buffer_uint,  MBevent& evt,std::ostream& out){
+    const size_t offset     = 3;
+    for(size_t count = 0 ; count < buffer_uint.size() ; ++count){
+        int chanNum    = (buffer_uint[offset+count]>>19) & 0x0000000F; // word #3 & up: read 4 bits
+        int window     = (buffer_uint[offset+count]>>17) & 0x00000003; // word #3 & up: read 2 bits
+
+        //----CHECK FOR SAMPLE -- INDICATED BY "BD" VALUE----//
+        // current usage is 31 bits
+        if(CHECK_FOR_SAMPLE(buffer_uint,offset,count,chanNum,window,evt)){
+            continue;
+        }
+        CHECK_FOR_FEATURE_EXTRACTION(buffer_uint,offset,count,evt);
+        CHECK_FOR_TRIGGER_BITS(buffer_uint,offset,count,out);
+
+    }//END OF EXTRACT DATA FROM CURRENT PACKAGE
+}
 //-----------------------------------------------------//
 //                   PROCESS BUFFER
 //-----------------------------------------------------//
@@ -138,123 +240,32 @@ void processBuffer(const unsigned char* buff,const long size, const char* wavefo
 
 
      //---- EXTRACT PACKAGES FROM BUFFER ----//
-    unsigned int buffer_uint[65536];
-    int pos1 = 0;
-    int pos2 = 0;
+    std::vector<unsigned int> buffer_uint(65536);
+
     int eventCounter = 1; // for printing progress to terminal
+    interval_t packet_interval(0,0);
 
 
-
-    while (pos1 < (size-16)) {
-
-        pos1 = FIND_BUFFER_POSITION_FOR_START_OF_PACKAGE(buff,pos1,size);
-        pos1 += 4; // Increment both place holders past package markers identifed above.
-
-        pos2 = FIND_BUFFER_POSITION_FOR_START_OF_PACKAGE(buff,pos1,size);
+    while (packet_interval.first < (size-16)) {
+        packet_interval = Find_package_in_buffer(buff,packet_interval,size);
 
 
-
-        //----CONVERT DATA FROM 8-BIT BUFFER TO 32-BIT BUFFER----//
-        if (!(buff[pos1]==0xFE && buff[pos1+4]==0xCF && (buff[pos1+8]&0xF0)==0xD0)) { // We dont have a package.
+        if (!is_valid_package(buff,packet_interval)) { // We dont have a package.
            continue;
         }
-        int wbuflen= CONVERT_DATA_FROM_8_BIT_BUFFER_TO_32_BIT_BUFFER( buff,pos1,pos2,buffer_uint);
+
+        packet_interval = CONVERT_DATA_FROM_8_BIT_BUFFER_TO_32_BIT_BUFFER( buff,packet_interval,buffer_uint);
     
     
-        PRINT_PSEUDO_STATUS_BAR_TO_TERMINAL(eventCounter);
-            
-            
+        PRINT_PSEUDO_STATUS_BAR_TO_TERMINAL(eventCounter++);
 
-        //----FOR EACH 32-bit WORD, EXTRACT DATA USING BIT-WISE & OPERATORS----//
-        evt.AddNum     =   (buffer_uint[0])      & 0x000001FF ;   // word #0: read 5 bits
-        evt.WrAddNum   =   (buffer_uint[0]>>9)   & 0x000001FF ;   // word #0: read 5 bits
-        evt.ASIC       =  ((buffer_uint[0]>>20)  & 0x0000000F)-1; // word #0: read 4 bits
-        evt.EvtNum     =   (buffer_uint[1])      & 0x00FFFFFF ;   // word #1: read 24 bits
-        evt.Wctime     =   (buffer_uint[2])      & 0x0FFFFFFF ;   // word #2: read 28 bits
-
-        //----PARSE UINT32 BUFFER & STORE DATA AS INSTANCE OF MBevent CLASS----//
-
-        int count       = 0;
-        const int offset     = 3;
-        int sampNum     = -1;
-        int chanNum     = -1;
-        int window      = -1;
+        extract_package_header(buffer_uint,evt);
+        extract_package_body(buffer_uint,evt,CAoutfile);
 
 
-
-        //----LOOP OVER CURRENT PACKAGE AND EXTRACT DATA----//
-        while (count < wbuflen) {
-            chanNum    = (buffer_uint[offset+count]>>19) & 0x0000000F; // word #3 & up: read 4 bits
-            window     = (buffer_uint[offset+count]>>17) & 0x00000003; // word #3 & up: read 2 bits
-
-
-            //----CHECK FOR SAMPLE -- INDICATED BY "BD" VALUE----//
-            // current usage is 31 bits
-            if ((buffer_uint[offset+count] & 0xFF000000) == 0xBD000000) {
-                sampNum     = ((buffer_uint[offset+count]>>12) & 0x0000001F)+window*32; // word #3 & up: read 5 bits
-                int BDval   =  (buffer_uint[offset+count]>>24) & 0x000000FF; // word #3 & up: read 8 bits (top 8 bits)
-                // make sure info is still valid before extracting samples---must pass these four conditions
-                if (evt.AddNum >= MEMORY_DEPTH  || evt.ASIC > NASICS || sampNum >= NSAMP || BDval != 0xBD) {
-                    cout << "INVALID SAMPLE INFO, SKIPPING "    << "\n"
-                         << "Loop counter: "    << count        << "\n"
-                         << "Address number: "  << evt.AddNum  << "\n"
-                         << "ASIC number: "     << evt.ASIC    << "\n"
-                         << "Sample number: "   << sampNum      << "\n"
-                         << "BD value: "        << BDval        << "\n";
-                    count++;
-                    continue;
-                }
-                else {
-                    evt.Sample[chanNum][sampNum] = ((buffer_uint[offset+count]) & 0x00000FFF); // word #3 & up: read 12 bits (bottom 12 bits)
-                }
-            }
-
-
-
-            CHECK_FOR_FEATURE_EXTRACTION(buffer_uint,offset,count,evt);
-
-
-
-            //----CHECK FOR TRIGGER BITS -- INDICATED BY "CA" WORD FOLLOWED BY "C7" WORD----//
-            const int n_ca_max=32; // total of 64 hex words, 32 each of "CAXXXXXX" and "C7XXXXXX"
-            int n_ca                         = 0;
-            unsigned int CA_val[n_ca_max]    = {0};
-                unsigned int CA_raw1[n_ca_max]   = {0};
-                unsigned int CA_raw2[n_ca_max]   = {0};
-                unsigned int CA_win[n_ca_max]    = {0};
-                unsigned int CA_tbs[n_ca_max][4] = {{0}};
-                // note: (CA)(23 downto 17)(7 downto 0) and (C7)(23 downto 20) are only stored in raw's
-                if ((buffer_uint[offset+count] & 0xFF000000) == 0xCA000000) {
-                    if ((buffer_uint[offset+count+1] & 0xFF000000) == 0xC7000000) {
-                        if (n_ca < n_ca_max) {
-                            CA_raw1[n_ca]   =  buffer_uint[offset+count  ]; // store "CA" word just in case
-                            CA_raw2[n_ca]   =  buffer_uint[offset+count+1]; // store "C7" word just in case
-                            CA_val[n_ca]    =  buffer_uint[offset+count+1] & 0x000FFFFF; // (C7) 19 downto 0 // redundant
-                            CA_tbs[n_ca][0] = (buffer_uint[offset+count+1]>>0 ) & 0x0000001F; // (C7) 4 downto 0
-                            CA_tbs[n_ca][1] = (buffer_uint[offset+count+1]>>5 ) & 0x0000001F; // (C7) 9 downto 5
-                            CA_tbs[n_ca][2] = (buffer_uint[offset+count+1]>>10) & 0x0000001F; // (C7) 14 downto 10
-                            CA_tbs[n_ca][3] = (buffer_uint[offset+count+1]>>15) & 0x0000001F; // (C7) 19 downto 15
-                            CA_win[n_ca]    = (buffer_uint[offset+count  ] & 0x0001FF00) >> 8; // (CA) 16 downto 8
-                            CAoutfile << CA_raw1[n_ca]   << "\t";
-                            CAoutfile << CA_raw2[n_ca]   << "\t";
-                            CAoutfile << CA_val[n_ca]    << "\t";
-                            CAoutfile << CA_win[n_ca]    << "\t";
-                            CAoutfile << CA_tbs[n_ca][0] << "\t";
-                            CAoutfile << CA_tbs[n_ca][1] << "\t";
-                            CAoutfile << CA_tbs[n_ca][2] << "\t";
-                            CAoutfile << CA_tbs[n_ca][3] << "\n";
-                        }
-                        n_ca++;
-                    }
-                }
-
-
-                count++;
-            }//END OF EXTRACT DATA FROM CURRENT PACKAGE
-
-            //----WRITE PARSED PACKAGE TO FILE (ONE LINE PER EVENT)----//
-            outfile << evt;
-            eventCounter++;
+        //----WRITE PARSED PACKAGE TO FILE (ONE LINE PER EVENT)----//
+        outfile << evt;
+       
        
     }//END OF ALL PACKAGES
     cout << "<---" << eventCounter-1 << "\n";
